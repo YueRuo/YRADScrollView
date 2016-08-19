1b54839c65941f9aa0ebb63dcf319c36f9ab249f
wangxiaoyu <wxy_yueruo@126.com>
2016-08-19 11:37:15 +0800

    增加页面出现和消失时的回调

--- YRADScrollView/YRADScrollView.h
+++ YRADScrollView/YRADScrollView.h
@@ -10,17 +10,17 @@
 @protocol YRADScrollViewDataSource;
 @protocol YRADScrollViewDelegate;
 @interface YRADScrollView : UIView
-@property (assign,nonatomic) NSInteger currentPage;
-@property (assign,nonatomic) BOOL scrollEnabled;//default is YES
-@property (assign,nonatomic) BOOL cycleEnabled;//是否可循环滚动，default is YES
-@property (weak,nonatomic) id<YRADScrollViewDataSource> dataSource;
-@property (weak,nonatomic) id<YRADScrollViewDelegate> delegate;
+@property (assign, nonatomic) NSInteger currentPage;
+@property (assign, nonatomic) BOOL scrollEnabled; //default is YES
+@property (assign, nonatomic) BOOL cycleEnabled;  //是否可循环滚动，default is YES
+@property (weak, nonatomic) id<YRADScrollViewDataSource> dataSource;
+@property (weak, nonatomic) id<YRADScrollViewDelegate> delegate;
 
--(id)dequeueReusableView;//重用池中取出一个控件
--(void)reloadData;
+- (id)dequeueReusableView; //重用池中取出一个控件
+- (void)reloadData;
 @end
 
-@protocol YRADScrollViewDataSource<NSObject>
+@protocol YRADScrollViewDataSource <NSObject>
 /*!
  *	@brief	获取数据源，要注意的是，使用dequeueReusableView进行获取，如果返回为nil，则再进行创建，类似tableView早前的数据获取方式。
  *
@@ -28,11 +28,13 @@
  *
  *	@return	要展示的控件
  */
--(UIView*)viewForYRADScrollView:(YRADScrollView*)adScrollView atPage:(NSInteger)pageIndex;
--(NSUInteger)numberOfViewsForYRADScrollView:(YRADScrollView*)adScrollView;
+- (UIView *)viewForYRADScrollView:(YRADScrollView *)adScrollView atPage:(NSInteger)pageIndex;
+- (NSUInteger)numberOfViewsForYRADScrollView:(YRADScrollView *)adScrollView;
 @end
 
-@protocol YRADScrollViewDelegate<NSObject>
--(void)adScrollView:(YRADScrollView*)adScrollView didClickedAtPage:(NSInteger)pageIndex;
--(void)adScrollView:(YRADScrollView*)adScrollView didScrollToPage:(NSInteger)pageIndex;
+@protocol YRADScrollViewDelegate <NSObject>
+- (void)adScrollView:(YRADScrollView *)adScrollView didClickedAtPage:(NSInteger)pageIndex;//点击了某一页
+- (void)adScrollView:(YRADScrollView *)adScrollView didScrollToPage:(NSInteger)pageIndex;//滚动到某一页
+- (void)adScrollView:(YRADScrollView *)adScrollView willDisplayView:(UIView *)view forPage:(NSInteger)pageIndex;//某页要出现了
+- (void)adScrollView:(YRADScrollView *)adScrollView didEndDisplayView:(UIView *)view forPage:(NSInteger)pageIndex;//某页要消失了
 @end
--- YRADScrollView/YRADScrollView.m
+++ YRADScrollView/YRADScrollView.m
@@ -8,11 +8,11 @@
 
 #import "YRADScrollView.h"
 
-@interface YRADScrollView ()<UIScrollViewDelegate>{
+@interface YRADScrollView () <UIScrollViewDelegate> {
     NSMutableSet *_reusableViewSet;
     NSMutableDictionary *_onShowViewDictionary;
     UIScrollView *_scrollView;
-    
+
     NSInteger _totalPageNumber;
     NSInteger _positionIndex;
 }
@@ -20,61 +20,59 @@
 @end
 @implementation YRADScrollView
 
--(id)init{
+- (instancetype)init {
     return [self initWithFrame:CGRectZero];
 }
-- (id)initWithFrame:(CGRect)frame
-{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        _reusableViewSet = [[NSMutableSet alloc]initWithCapacity:4];
-        _onShowViewDictionary = [[NSMutableDictionary alloc]initWithCapacity:3];
+        _reusableViewSet = [[NSMutableSet alloc] initWithCapacity:4];
+        _onShowViewDictionary = [[NSMutableDictionary alloc] initWithCapacity:3];
         _cycleEnabled = true;
-        
-        _scrollView = [[UIScrollView alloc]initWithFrame:self.bounds];
+
+        _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
         _scrollView.pagingEnabled = true;
         _scrollView.delegate = self;
         _scrollView.showsHorizontalScrollIndicator = false;
         _scrollView.showsVerticalScrollIndicator = false;
         [self addSubview:_scrollView];
-        
-        
+
         UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
         [_scrollView addGestureRecognizer:gesture];
     }
     return self;
 }
 
--(void)layoutSubviews{
+- (void)layoutSubviews {
     _scrollView.frame = self.bounds;
     [self reloadData];
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
--(void)setScrollEnabled:(BOOL)scrollEnabled{
+- (void)setScrollEnabled:(BOOL)scrollEnabled {
     _scrollView.scrollEnabled = scrollEnabled;
 }
 
--(BOOL)scrollEnabled{
+- (BOOL)scrollEnabled {
     return _scrollView.scrollEnabled;
 }
 
--(void)handleTap:(UITapGestureRecognizer*)gesture{
-    if (self.delegate&&[self.delegate respondsToSelector:@selector(adScrollView:didClickedAtPage:)]) {
+- (void)handleTap:(UITapGestureRecognizer *)gesture {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adScrollView:didClickedAtPage:)]) {
         [self.delegate adScrollView:self didClickedAtPage:_currentPage];
     }
 }
 
--(id)dequeueReusableView{
+- (id)dequeueReusableView {
     id obj = [_reusableViewSet anyObject];
     if (obj) {
         [_reusableViewSet removeObject:obj];
@@ -82,105 +80,113 @@
     return obj;
 }
 
--(void)reloadData{
+- (void)reloadData {
     if (self.dataSource) {
         if ([self.dataSource respondsToSelector:@selector(numberOfViewsForYRADScrollView:)]) {
             _totalPageNumber = [self.dataSource numberOfViewsForYRADScrollView:self];
         }
     }
-    if (_onShowViewDictionary.count>0) {
+    if (_onShowViewDictionary.count > 0) {
         [_reusableViewSet addObjectsFromArray:[_onShowViewDictionary allValues]];
         [_onShowViewDictionary removeAllObjects];
     }
-    
+
     if (_cycleEnabled) {
-        _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width*2000*_totalPageNumber, 0);
-        _positionIndex = 1000*_totalPageNumber+self.currentPage;
-        [_scrollView setContentOffset:CGPointMake(_positionIndex*_scrollView.frame.size.width, 0) animated:false];
-    }else{
-        _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width*_totalPageNumber, 0);
+        _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width * 2000 * _totalPageNumber, 0);
+        _positionIndex = 1000 * _totalPageNumber + self.currentPage;
+        [_scrollView setContentOffset:CGPointMake(_positionIndex * _scrollView.frame.size.width, 0) animated:false];
+    } else {
+        _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width * _totalPageNumber, 0);
         _positionIndex = self.currentPage;
-        [_scrollView setContentOffset:CGPointMake(_positionIndex*_scrollView.frame.size.width, 0) animated:false];
+        [_scrollView setContentOffset:CGPointMake(_positionIndex * _scrollView.frame.size.width, 0) animated:false];
     }
 
-    if (_totalPageNumber>0) {
+    if (_totalPageNumber > 0) {
         [self setPageToPositionIndex:_positionIndex];
     }
 }
 
--(void)setPageToPositionIndex:(NSInteger)positionIndex{
+- (void)setPageToPositionIndex:(NSInteger)positionIndex {
     [self prepareViewAtPositionIndex:positionIndex];
-    [self prepareViewAtPositionIndex:positionIndex-1];
-    [self prepareViewAtPositionIndex:positionIndex+1];
-    
+    [self prepareViewAtPositionIndex:positionIndex - 1];
+    [self prepareViewAtPositionIndex:positionIndex + 1];
+
     NSArray *allKeyArray = _onShowViewDictionary.allKeys;
-    for (NSInteger i=allKeyArray.count-1;i>=0;i--) {
+    for (NSInteger i = allKeyArray.count - 1; i >= 0; i--) {
         NSNumber *key = [allKeyArray objectAtIndex:i];
         NSInteger index = [key integerValue];
         UIView *view = [_onShowViewDictionary objectForKey:key];
-        if (ABS(index-positionIndex)>1) {
+        if (ABS(index - positionIndex) > 1) {
             view.hidden = true;
             [_reusableViewSet addObject:view];
             [_onShowViewDictionary removeObjectForKey:key];
-        }else{
+        } else {
             view.hidden = false;
         }
     }
+    if ([self.delegate respondsToSelector:@selector(adScrollView:willDisplayView:forPage:)]) {
+        UIView *view = [_onShowViewDictionary objectForKey:@(positionIndex)];
+        [self.delegate adScrollView:self willDisplayView:view forPage:self.currentPage];
+    }
 }
 
--(NSInteger)pageFromPositionIndex:(NSInteger)positionIndex{
-    if (_totalPageNumber==0) {
+- (NSInteger)pageFromPositionIndex:(NSInteger)positionIndex {
+    if (_totalPageNumber == 0) {
         return 0;
     }
     NSInteger showIndex = positionIndex;
-    if (positionIndex>0) {
-        showIndex = positionIndex%_totalPageNumber;
-    }else if(positionIndex<0){
-        showIndex = positionIndex%_totalPageNumber+_totalPageNumber;
+    if (positionIndex > 0) {
+        showIndex = positionIndex % _totalPageNumber;
+    } else if (positionIndex < 0) {
+        showIndex = positionIndex % _totalPageNumber + _totalPageNumber;
     }
     return showIndex;
 }
 
--(void)prepareViewAtPositionIndex:(NSInteger)positionIndex{
+- (void)prepareViewAtPositionIndex:(NSInteger)positionIndex {
     if (!_cycleEnabled) {
-        if (positionIndex<0||positionIndex>_totalPageNumber-1) {
+        if (positionIndex < 0 || positionIndex > _totalPageNumber - 1) {
             return;
         }
     }
     NSInteger showIndex = [self pageFromPositionIndex:positionIndex];
     UIView *view = [_onShowViewDictionary objectForKey:@(positionIndex)];
-    if (!view&&self.dataSource&&[self.dataSource respondsToSelector:@selector(viewForYRADScrollView:atPage:)]) {
+    if (!view && self.dataSource && [self.dataSource respondsToSelector:@selector(viewForYRADScrollView:atPage:)]) {
         view = [self.dataSource viewForYRADScrollView:self atPage:showIndex];
         [_scrollView addSubview:view];
         [_onShowViewDictionary setObject:view forKey:@(positionIndex)];
     }
-    view.frame = CGRectMake(positionIndex*_scrollView.frame.size.width, 0, _scrollView.frame.size.width, _scrollView.frame.size.height);
+    view.frame = CGRectMake(positionIndex * _scrollView.frame.size.width, 0, _scrollView.frame.size.width, _scrollView.frame.size.height);
     view.hidden = false;
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    if (_totalPageNumber==0) {
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (_totalPageNumber == 0) {
         return;
     }
     CGFloat pageWidth = _scrollView.frame.size.width;
-    NSInteger page = (_scrollView.contentOffset.x/pageWidth) + 0.5;
-    if (page!=_positionIndex) {
+    NSInteger page = (_scrollView.contentOffset.x / pageWidth) + 0.5;
+    if (page != _positionIndex) {
         if (!_cycleEnabled) {
-            if (page<0||page>_totalPageNumber-1) {
+            if (page < 0 || page > _totalPageNumber - 1) {
                 return;
             }
         }
+        if ([self.delegate respondsToSelector:@selector(adScrollView:didEndDisplayView:forPage:)]) {
+            UIView *view = [_onShowViewDictionary objectForKey:@(_positionIndex)];
+            [self.delegate adScrollView:self didEndDisplayView:view forPage:_positionIndex];
+        }
         _positionIndex = page;
         _currentPage = [self pageFromPositionIndex:_positionIndex];
-        if (self.delegate&&[self.delegate respondsToSelector:@selector(adScrollView:didScrollToPage:)]) {
+        if (self.delegate && [self.delegate respondsToSelector:@selector(adScrollView:didScrollToPage:)]) {
             [self.delegate adScrollView:self didScrollToPage:_currentPage];
         }
 
         [self setPageToPositionIndex:_positionIndex];
     }
 }
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
-    
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
 }
 
 @end
